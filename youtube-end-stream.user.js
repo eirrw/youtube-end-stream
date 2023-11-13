7a62707b4af80933b4e07f722996e7051f7e2231
Eir Wilson <git@eirrw.com>
2023-11-13 14:34:21 -0500

    adjust logging and constants

--- youtube-end-stream.user.js
+++ youtube-end-stream.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        YouTube End Stream
 // @namespace   eirrw
-// @version     1.0.0
+// @version     1.0.1
 // @description Automatically dismiss the popup when ending a stream on YouTube
 // @author      eirrw
 // @match       https://studio.youtube.com/*/livestreaming
@@ -11,7 +11,9 @@
 
 (function () {
     'use strict';
-    const DEBUG = false;
+    const LOG_STATUS = true;        // output status logs
+    const LOG_DEBUG = false;        // log debug info
+    const POLL_TIME_SECONDS = 30;   // time between checks in seconds
 
     const DISMISS_LINK_SELECTOR = '#dismiss-link';
     const END_STREAM_SELECTOR = '#end-stream-button';
@@ -23,7 +25,7 @@
 
         // debug info, set const to use
         let debugInfo = {};
-        if (DEBUG) {
+        if (LOG_DEBUG) {
             debugInfo = {
                 element: el,
                 isVisible: (el?.offsetParent !== null),
@@ -34,16 +36,16 @@
 
         // attempt to trigger popup if not active
         if (el === null || el.offsetParent === null) {
-            console.log('Dismiss link not available', debugInfo);
+            (LOG_DEBUG || LOG_STATUS) && console.log('Dismiss link not available', debugInfo);
             window.focus();
             return;
         }
 
         // click the button
-        console.log('Found dismiss link, triggering', debugInfo);
+        (LOG_DEBUG || LOG_STATUS) && console.log('Found dismiss link, triggering', debugInfo);
         el.click();
     }
 
     console.log('Starting dismiss watcher');
-    setInterval(triggerDismissLink, 30000); // 30 seconds
+    setInterval(triggerDismissLink, (POLL_TIME_SECONDS * 1000)); // start poll timer
 })();
